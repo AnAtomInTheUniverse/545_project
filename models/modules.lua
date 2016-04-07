f2d9c299709af9df9cfd53a52ba06e979bb2cfc1
Dominic Calabrese <dcalabre@umich.edu>
2016-04-07 16:20:58 -0400

    Fixed indexing problem in for-loop of CycSlice module

--- models/modules.lua
+++ models/modules.lua
@@ -8,26 +8,15 @@ function CycSlice:updateOutput(input)
 	local rot = torch.eye(input:size(4))
 	rot = image.hflip(rot)
 	rot = rot:repeatTensor(input:size(2),1,1)
-	
-	self.output = torch.Tensor(input:size(1)*4,input:size(2),input:size(3),input:size(4))
+	local oldBatch=input:size(1)
+	self.output = torch.Tensor(oldBatch*4,input:size(2),input:size(3),input:size(4))
 
-	for i = 1,input:size(1) do
+	for i = 1,oldBatch do
 		self.output[{i,{},{},{}}] = input[{i,{},{},{}}]
-		self.output[{4 + i,{},{},{}}] = torch.bmm(input[{i,{},{},{}}]:transpose(2,3),rot)
-		self.output[{8 + i,{},{},{}}] = torch.bmm(rot,torch.bmm(input[{i,{},{},{}}],rot))
-		self.output[{12 + i,{},{},{}}] = torch.bmm(rot,input[{i,{},{},{}}]:transpose(2,3))
+		self.output[{oldBatch + i,{},{},{}}] = torch.bmm(input[{i,{},{},{}}]:transpose(2,3),rot)
+		self.output[{2*oldBatch + i,{},{},{}}] = torch.bmm(rot,torch.bmm(input[{i,{},{},{}}],rot))
+		self.output[{3*oldBatch + i,{},{},{}}] = torch.bmm(rot,input[{i,{},{},{}}]:transpose(2,3))
 	end
-	--[[	
-	local input90 = torch.zeros(input:size())
-	local input180 = torch.zeros(input:size())
-	local input270 = torch.zeros(input:size())
-	for i = 1,input:size(1) do
-		input90[{i,{},{},{}}] = torch.bmm(input[{i,{},{},{}}]:transpose(2,3),rot)
-		input180[{i,{},{},{}}] = torch.bmm(input90[{i,{},{},{}}]:transpose(2,3),rot)
-		input270[{i,{},{},{}}] = torch.bmm(input180[{i,{},{},{}}]:transpose(2,3),rot)
-	end
-	--output = torch.cat({input,input90,input180,input270},1)
-	self.output:cat({input,input90,input180,input270},1) --]]
 	return self.output
 end
 
