4174521ab5d371f68b4ce5bfff33da6845138dde
Amlan Nayak <amlan@umich.edu>
2016-04-07 16:00:35 -0400

    confusion matrix addded to get_acc and changed batch size in output

--- src/get_acc.lua
+++ src/get_acc.lua
@@ -4,6 +4,9 @@
 -- models and data from ../models/ and ../data/
 -- ]]
 require 'torch'
+require 'optim'
+
+classes = {'0','1','2','3','4','5','6','7','8','9'}
 
 cmd = torch.CmdLine()
 cmd:option('-p', 'predicted label file')
@@ -15,6 +18,13 @@ true_temp = torch.load(args.t)
 
 pred = torch.DoubleTensor(pred_temp:size()):copy(pred_temp)
 true_labels = torch.DoubleTensor(true_temp:size()):copy(true_temp)
+confusion = optim.ConfusionMatrix(classes)
 
+for i = 1,pred:size(1) do
+	local temp = torch.zeros(10)
+	temp[pred[i][1]+1] = 1
+	confusion:add(temp,true_labels[i][1]+1)
+end
+print(confusion)
 acc = torch.sum(torch.eq(pred, true_labels))/50000
 print("Accuracy: " .. acc)
