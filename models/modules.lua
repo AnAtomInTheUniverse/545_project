03a2263fdefc26c625f1d82aac4513a54942cbf0
Dominic Calabrese <dcalabre@umich.edu>
2016-04-16 20:00:19 -0400

    Create version of mean pooling without rotations

--- models/modules.lua
+++ models/modules.lua
@@ -16,8 +16,8 @@ function CycSlice:updateOutput(input)
 		self.output[{i,{},{},{}}] = input[{i,{},{},{}}]
 		self.output[{batch + i,{},{},{}}] = torch.bmm(input[{i,{},{},{}}]:transpose(2,3),rot) --perform 90 rotation
 		self.output[{2*batch + i,{},{},{}}] = torch.bmm(rot,torch.bmm(input[{i,{},{},{}}],rot)) --perform 180 rotation
-		self.output[{3*batch + i,{},{},{}}] = torch.bmm(rot,input[{i,{},{},{}}]:transpose(2,3)) -- perform 270 rotation
-	end
+		self.output[{3*batch + i,{},{},{}}] = torch.bmm(rot,input[{i,{},{},{}}]:transpose(2,3)) --perform 270 rotation
+	end 
 	return self.output
 end
 
@@ -68,6 +68,42 @@ function MeanCycPool:updateGradInput(input,gradOutput)
 		self.gradInput[{batch + i,{},{},{}}] = torch.bmm(gradOutput[{i,{},{},{}}]:transpose(2,3),rot)/4 --perform 90 rotation
 		self.gradInput[{2*batch + i,{},{},{}}] = torch.bmm(rot,torch.bmm(gradOutput[{i,{},{},{}}],rot))/4 -- perform 180 rotation
 		self.gradInput[{3*batch + i,{},{},{}}] = torch.bmm(rot,gradOutput[{i,{},{},{}}]:transpose(2,3))/4 --perform 270 rotation
+	end
+	return self.gradInput
+end
+
+----------------------------------------------------------------
+
+local MeanCycPool2 = torch.class('nn.MeanCycPool2', 'nn.Module')
+
+function MeanCycPool2:updateOutput(input)
+	local rot = torch.eye(input:size(4))
+	rot = image.hflip(rot)
+	rot = rot:repeatTensor(input:size(2),1,1)
+	local batch = input:size(1)/4
+	self.output = torch.zeros(batch,input:size(2),input:size(3),input:size(4))
+	for i = 1,batch do
+		self.output[{i,{},{},{}}] = (input[{i,{},{},{}}]	
+				+ input[{batch + i,{},{},{}}] 
+				+ input[{batch*2 + i,{},{},{}}] --perform 180 rotation
+				+ input[{batch*3 + i,{},{},{}}])/4 --perform 90 rotation
+	end
+	--self.output:copy(output)
+	return self.output
+end
+
+function MeanCycPool2:updateGradInput(input,gradOutput)
+	local rot = torch.eye(gradOutput:size(4))
+	rot = image.hflip(rot)
+	rot = rot:repeatTensor(gradOutput:size(2),1,1)	
+	--self.gradInput:copy(input)
+	local batch = gradOutput:size(1)
+	self.gradInput = torch.zeros(input:size())
+	for i = 1,batch do 
+		self.gradInput[{i,{},{},{}}] = gradOutput[{i,{},{},{}}]/4
+		self.gradInput[{batch + i,{},{},{}}] = gradOutput[{i,{},{},{}}]/4 --perform 90 rotation
+		self.gradInput[{2*batch + i,{},{},{}}] = gradOutput[{i,{},{},{}}]/4 -- perform 180 rotation
+		self.gradInput[{3*batch + i,{},{},{}}] = gradOutput[{i,{},{},{}}]/4 --perform 270 rotation
 	end
 	return self.gradInput
 end
